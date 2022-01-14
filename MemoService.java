4d509b2bfc575807cc3133c4b2bf92555f26fc6a
Seunghwan Jung <67155560+hwan2dang@users.noreply.github.com>
2022-01-14 14:10:25 -0500

    Update file

---
+++ MemoService.java
@@ -0,0 +1,28 @@
+package com.timeline.timeline.service;
+
+import com.timeline.timeline.domain.Memo;
+import com.timeline.timeline.domain.MemoRepository;
+import com.timeline.timeline.domain.MemoRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class MemoService {
+
+    private final MemoRepository memoRepository;
+
+    @Transactional
+    public Long update(Long id, MemoRequestDto requestDto) {
+        Memo memo = memoRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("아이디가 존재하지 않습니다.")
+        );
+        memo.update(requestDto);
+        return memo.getId();
+    }
+
+}
+
+
